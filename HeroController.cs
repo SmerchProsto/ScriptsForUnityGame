80f72b6737d96e31e8d94b1fd56971ded0cc382f
Smerch <smerchprosto@gmail.com>
2020-05-02 17:10:03 +0400

    Написал анимацию нормальной атаки

--- HeroController.cs
+++ HeroController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class HeroController : MonoBehaviour
 {
-  bool IsDead, IsRight, IsGround;
+  bool isDead, isRight, isGround;
+  int attackCost;
   public int speed = 5, jumpForce = 50;
   public LayerMask GroundLayer;
   public Transform GroundCheck;
@@ -14,9 +15,11 @@ public class HeroController : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-    IsRight = true;
-    IsDead = false;
-    IsGround = true;
+    isRight = true;
+    isDead = false;
+    isGround = true;
+
+    attackCost = 0;
 
     rb = GetComponent<Rigidbody2D>();
     anim = GetComponent<Animator>();
@@ -26,13 +29,14 @@ public class HeroController : MonoBehaviour
   void Update()
   {
     CheckHeroIsAlive();
-    CheckPressedButton();
     CheckHeroOnGround();
+    CheckPressedButton();
+    
   }
 
   void CheckHeroIsAlive()
   {
-    if (!IsDead)
+    if (!isDead)
     {
       Time.timeScale = 1f;
     }
@@ -46,7 +50,7 @@ public class HeroController : MonoBehaviour
   {
     if (Input.GetKey(KeyCode.D))
     {
-      if (Input.GetAxis("Horizontal") > 0 && !IsRight)
+      if (Input.GetAxis("Horizontal") > 0 && !isRight)
       {
         Flip();
       }
@@ -55,7 +59,7 @@ public class HeroController : MonoBehaviour
     }
     else if (Input.GetKey(KeyCode.A))
     {
-      if (Input.GetAxis("Horizontal") < 0 && IsRight)
+      if (Input.GetAxis("Horizontal") < 0 && isRight)
       {
         Flip();
       }
@@ -67,20 +71,37 @@ public class HeroController : MonoBehaviour
       anim.SetBool("IsRunning", false);
     }
 
-    if (Input.GetKey(KeyCode.W) && IsGround)
+    if (Input.GetKey(KeyCode.W) && isGround)
     {
       anim.SetTrigger("Jump");
       rb.AddForce(new Vector2(0, jumpForce));
     }
+
+    if (Input.GetKeyDown(KeyCode.J))
+    {
+      attackCost++;
+      if (attackCost == 3) 
+      {
+        attackCost = 1;
+      }
+
+      anim.SetInteger("NormalAttack", attackCost);
+      anim.SetBool("HeroIsNormalAttacking", true);
+    }
+    else
+    {
+      anim.SetBool("HeroIsNormalAttacking", false);
+    }
+
 
     transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
   }
 
   void CheckHeroOnGround()
   {
-    IsGround = Physics2D.OverlapArea(new Vector2(GroundCheck.position.x - 0.4f, GroundCheck.position.y - 0.2f), new Vector2(GroundCheck.position.x + 0.4f, GroundCheck.position.y + 0.2f), GroundLayer);
+    isGround = Physics2D.OverlapArea(new Vector2(GroundCheck.position.x - 0.4f, GroundCheck.position.y - 0.2f), new Vector2(GroundCheck.position.x + 0.4f, GroundCheck.position.y + 0.2f), GroundLayer);
 
-    if (IsGround)
+    if (isGround)
     {
       anim.SetBool("IsFalling", false);
     }
@@ -92,7 +113,7 @@ public class HeroController : MonoBehaviour
 
   void Flip()
   {
-    IsRight = !IsRight;
+    isRight = !isRight;
     Vector3 NewScale = transform.localScale;
     NewScale.x *= -1;
     transform.localScale = NewScale;
